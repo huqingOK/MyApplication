ec90240e8b0c5cf0ed86590603c75ed9f1844f71
506966949@qq.com <huqing1992>
2016-11-03 16:04:46 +0800

    Initial commit

---
+++ app/src/main/java/information/safe/anyi/com/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package information.safe.anyi.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
